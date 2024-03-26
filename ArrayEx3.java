fd2f752bce055789f676b925fa2d36ff0fef4e04
VitorHSR18 <136871451+VitorHSR18@users.noreply.github.com>
2024-03-26 20:41:40 -0300

    Create ArrayEx3.java

---
+++ ArrayEx3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+
+public class Main {
+    public static int[][] preencherVetor(int l,int c,int min,int max){
+        int vet[][],i,j;
+        vet = new int[l][c];
+        for (i=0;i<l;i++) {
+            for (j = 0; j < c; j++) {
+                vet[i][j] = (int) (Math.random() * (max - min) + min);
+            }
+        }
+        return vet;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int l,c,min,max,i,j;
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o tamanho , minimo e o maximo do vetor");
+        l = leitor.nextInt();
+        c = leitor.nextInt();
+        min = leitor.nextInt();
+        max = leitor.nextInt();
+        int vet[][]=preencherVetor(l,c,min,max);
+
+        for(i=0;i<l;i++)
+            for(j=0;j<c;j++)
+                {
+                    System.out.printf("%d \n",vet[i][j]);
+                }
+        for(i=0;i<l;i++)
+            for(j=0;j<c;j++) {
+
+                if(vet[i][j]<l*c)
+                    System.out.println(false);
+                else System.out.println(true);
+            }
+    }
+    }
